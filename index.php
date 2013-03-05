0525e56d8b3ba6c053f7a372bf3d2850c0975060
jason kuhn <spaceyraygun@gmail.com>
2013-03-05 18:50:52 -0500

    testing blockquote

--- index.php
+++ index.php
@@ -32,17 +32,23 @@
         
         <li class="skills">
             <h3>Make</h3>
+            <blockquote>
             <p>I do <a href="https://github.com/jklmnop" title="my public github repos">web stuff</a> with <span class="_" title="html, css, js, php, sql">languages</span> <span class="hide">(HTML, CSS, JS, PHP, SQL)</span>, <span class="_" title="jquery, yui, zend, 960">frameworks</span> <span class="hide">(jQuery, YUI, Zend, 960)</span>, <abbr title="and per se and" class="amp">&amp;</abbr> <span class="_" title="wordpress, drupal">content management systems</span> <span class="hide">(Wordpress, Drupal)</span>.<p>
+            </blockquote>
         </li>
         
         <li class="education">
             <h3>Think</h3>
+            <blockquote>
             <p>I undergrad-ed at an <a href="http://www.artinstitutes.edu/philadelphia/" title="art institute of philadelphia">art school</a> <abbr title="and per se and" class="amp">&amp;</abbr> now I'm grad-ing at a <a href="http://www.ischool.drexel.edu/" title="drexel university's college of science and technology">technology school</a>.<p>
+            </blockquote>
         </li>
         
         <li class="hobbies">
             <h3>Play</h3>
+            <blockquote>
             <p>I live in philadelphia. I ride <a href="http://i.imgur.com/KPb9c.jpg" title="my univega">my bicycle</a>, I make <a href="http://spaceyraygun.net" title="spaceyraygun on muxtape">noise</a>, <abbr title="and per se and" class="amp">&amp;</abbr> I watch <a href="http://www.youtube.com/show/macgyver" title="this attribute is unnecessary">MacGyver</a>.<p>
+            </blockquote>
         </li>
     </ul>    
 </div>
