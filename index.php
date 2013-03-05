2567e48ddf1eeede2320871abb989228f1851f82
jason kuhn <spaceyraygun@gmail.com>
2013-03-05 18:40:45 -0500

    some scaffolding

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html public "display of affection">
-<head>
+<head lang="en">
   <meta charset="utf-8">
 
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -15,7 +15,7 @@
 
 <body>
 
-<div id="wrapper" class="">
+<div id="wrapper" class="container">
     <div id="whatsmyname" class="">
         <h1 class="initials"><abbr title="Jason Kuhn">JK</abbr></h1>
         <h2 class="bio">is <small>short</small> for <em class="not_name">just kidding</em> <abbr title="and per se and" class="amp">&amp;</abbr> <em class="name">Jason K&uuml;hn</em>.</h2>
@@ -24,23 +24,23 @@
         <?php endif; ?>
     </div>
     
-    <ul id="whoami" class="">
+    <ul id="whoami" class="row">
       <li class="employment">
             <h3>Work</h3>
             <p>I used to make things for a <a href="http://www.lockheedmartin.com/isgs/" title="lockheed martin information systems and global services">defense contractor</a> <abbr title="and per se and" class="amp">&amp;</abbr> now I make things for <a href="http://lebow.drexel.edu" title="drexel university's lebow college of business">academia</a>.<p>
         </li>
         
-        <li class="skills">
+        <li class="skills span4">
             <h3>Make</h3>
             <p>I do <a href="https://github.com/jklmnop" title="my public github repos">web stuff</a> with <span class="_" title="html, css, js, php, sql">languages</span> <span class="hide">(HTML, CSS, JS, PHP, SQL)</span>, <span class="_" title="jquery, yui, zend, 960">frameworks</span> <span class="hide">(jQuery, YUI, Zend, 960)</span>, <abbr title="and per se and" class="amp">&amp;</abbr> <span class="_" title="wordpress, drupal">content management systems</span> <span class="hide">(Wordpress, Drupal)</span>.<p>
         </li>
         
-        <li class="education">
+        <li class="education span4">
             <h3>Think</h3>
             <p>I undergrad-ed at an <a href="http://www.artinstitutes.edu/philadelphia/" title="art institute of philadelphia">art school</a> <abbr title="and per se and" class="amp">&amp;</abbr> now I'm grad-ing at a <a href="http://www.ischool.drexel.edu/" title="drexel university's college of science and technology">technology school</a>.<p>
         </li>
         
-        <li class="hobbies">
+        <li class="hobbies span4">
             <h3>Play</h3>
             <p>I live in philadelphia. I ride <a href="http://i.imgur.com/KPb9c.jpg" title="my univega">my bicycle</a>, I make <a href="http://spaceyraygun.net" title="spaceyraygun on muxtape">noise</a>, <abbr title="and per se and" class="amp">&amp;</abbr> I watch <a href="http://www.youtube.com/show/macgyver" title="this attribute is unnecessary">MacGyver</a>.<p>
         </li>
