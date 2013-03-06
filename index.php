7ef9e5301b190b27d2f5ca53d3ee9b5f60aef50a
jason kuhn <spaceyraygun@gmail.com>
2013-03-05 19:09:34 -0500

    styled the forms

--- index.php
+++ index.php
@@ -55,15 +55,15 @@
     <div id="contact" class="">
         <form id="twitter" action="twitter.php" method="post">
         <fieldset><legend><span class="caps">Message me on Twitter</span> or <a href="#email" class="toggle">by E-mail</a></legend>
-        <p class="hint">Please use up to 130 characters (140 minus @).</p>
+        <p class="hint help-block">Please use up to 130 characters (140 minus @).</p>
         <ol>
             <li>
-                <label for="status" class="hide">Message: </label>
-              <div class="ta-wrap"><textarea id="status" name="status" cols="40" rows="5" maxlength="130"></textarea></div>
+              <label for="status" class="hide">Message: </label>
+              <div class="ta-wrap span12"><textarea id="status" name="status" cols="40" rows="5" maxlength="130"></textarea></div>
                 
             </li>
             <li class="buttons">
-                <input type="submit" class="submit" value="tweet @jklmnop" /> or <input type="reset" class="reset" value="don't." />
+                <input type="submit" class="submit btn btn-primary" value="tweet @jklmnop" /> or <input type="reset" class="reset btn btn-link" value="don't." />
             </li>
         </ol>
 
@@ -77,10 +77,10 @@
         <ol>
             <li>
                 <label for="message" class="hide">Message: </label>
-              <div class="ta-wrap"><textarea id="message" name="message" cols="40" rows="5"></textarea></div>
+              <div class="ta-wrap span12"><textarea id="message" name="message" cols="40" rows="5"></textarea></div>
             </li>
             <li class="buttons">
-                <input type="submit" class="submit" value="send e-mail" /> or <input type="reset" class="reset" value="don't." />
+                <input type="submit" class="submit btn btn-primary" value="send e-mail" /> or <input type="reset" class="reset btn btn-link" value="don't." />
             </li>
         </ol>
         <p class="oldfashioned">or the old way &mdash; <a href="mailto:spaceyraygun@gmail.com">spaceyraygun@gmail.com</a></p>
