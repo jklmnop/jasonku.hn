f638b5a4b03a80e57bfb205e5a9805f3bad29ede
jason kuhn <spaceyraygun@gmail.com>
2013-03-06 16:07:18 -0500

    content changes

--- index.php
+++ index.php
@@ -26,72 +26,48 @@
         <article class="employment span3">
             <h3>Work</h3>
             <blockquote>
-            <p>I used to make things for a <a href="http://www.lockheedmartin.com/isgs/" title="lockheed martin information systems and global services">defense contractor</a> <abbr title="and per se and" class="amp">&amp;</abbr> now I make things for <a href="http://lebow.drexel.edu" title="drexel university's lebow college of business">academia</a>.</p>
+            <p>I used to make things for a defense contractor <abbr title="and per se and" class="amp">&amp;</abbr> now I make things for academia.</p>
             </blockquote>
+            <ul>
+                <li><a href="http://www.lockheedmartin.com/isgs/" title="lockheed martin information systems and global services">Lockheed Martin</a></li>
+                <li><a href="http://lebow.drexel.edu" title="drexel university's lebow college of business">Drexel LeBow</a></li>
+            </ul>
         </article>
         
         <article class="skills span3">
             <h3>Make</h3>
             <blockquote>
-            <p>I do <a href="https://github.com/jklmnop" title="my public github repos">web stuff</a> with <span class="_" title="html, css, js, php, sql">languages</span> <span class="hide">(HTML, CSS, JS, PHP, SQL)</span>, <span class="_" title="jquery, yui, zend, 960">frameworks</span> <span class="hide">(jQuery, YUI, Zend, 960)</span>, <abbr title="and per se and" class="amp">&amp;</abbr> <span class="_" title="wordpress, drupal">content management systems</span> <span class="hide">(Wordpress, Drupal)</span>.</p>
+            <p>I do web stuff with <span class="_" title="html, css, js, php, sql">languages</span> <span class="hide">(HTML, CSS, JS, PHP, SQL)</span>, <span class="_" title="jquery, yui, zend, 960">frameworks</span> <span class="hide">(jQuery, YUI, Zend, 960)</span>, <abbr title="and per se and" class="amp">&amp;</abbr> <span class="_" title="wordpress, drupal">content management systems</span> <span class="hide">(Wordpress, Drupal)</span>.</p>
             </blockquote>
+            <ul>
+                <li><a href="http://github.com/jklmnop" title="my public github repos">GitHub</a></li>
+                <li><a href="http://drupal.org/user/779688" title="my drupal.org user page">Drupal</a></li>
+            </ul>
         </article>
         
         <article class="education span3">
             <h3>Think</h3>
             <blockquote>
-            <p>I undergrad-ed at an <a href="http://www.artinstitutes.edu/philadelphia/" title="art institute of philadelphia">art school</a>, then I grad-ed at a <a href="http://www.ischool.drexel.edu/" title="drexel university's college of science and technology">technology school</a> but now I'm self-educating.</p>
+            <p>I undergrad-ed at an art school, then I grad-ed at a technology school but now I'm self-educating.</p>
             </blockquote>
+            <ul>
+                <li><a href="http://www.artinstitutes.edu/philadelphia/" title="art institute of philadelphia">AiPH</a></li>
+                <li><a href="http://www.ischool.drexel.edu/" title="drexel university's college of science and technology">iSchool</a></li>
+            </ul>
         </article>
         
         <article class="hobbies span3">
             <h3>Play</h3>
             <blockquote>
-            <p>I live in philadelphia. I ride <a href="http://i.imgur.com/KPb9c.jpg" title="my univega">my bicycle</a>, I make <a href="http://spaceyraygun.net" title="spaceyraygun on bandcamp">noise</a>, <abbr title="and per se and" class="amp">&amp;</abbr> I watch <a href="http://www.youtube.com/show/macgyver" title="this attribute is unnecessary">MacGyver</a>.</p>
+            <p>I live in Philadelphia. I ride my bicycle, I make noise</a>, <abbr title="and per se and" class="amp">&amp;</abbr> I watch MacGyver.</p>
             </blockquote>
+            <ul>
+                <li><a href="http://spaceyraygun.net" title="spaceyraygun on bandcamp">spaceyraygun</a></li>
+                <li><a href="http://locketsmusic.com">Lockets</a></li>
+            </ul>
         </article>
     </section>
-    
-    <!--div id="contact" class="row">
-        <form id="twitter" action="twitter.php" method="post" class="span6">
-        <fieldset><legend><span class="caps">Message me on Twitter</span> or <a href="#email" class="toggle">by E-mail</a></legend>
-        <p class="hint help-block">Please use up to 130 characters (140 minus @).</p>
-        <ol class="inline">
-            <li>
-              <label for="status" class="hide">Message: </label>
-              <div class="ta-wrap"><textarea id="status" name="status" cols="40" rows="5" maxlength="130"></textarea></div>
-                
-            </li>
-            <li class="buttons">
-                <input type="submit" class="submit btn btn-primary" value="tweet @jklmnop" /> or <input type="reset" class="reset btn btn-link" value="don't." />
-            </li>
-        </ol>
 
-        </fieldset>
-        </form>
-        
-        <form id="email" action="email.php" method="post" class="span6">
-        <fieldset><legend><span class="caps">E-Mail me</span> or <a href="#twitter" class="toggle">send me a Tweet</a></legend>
-        <p class="whatever help-block">Put some contact info in your message or don't. It's entirely up to you.</p>
-        <p class="hint"></p>
-        <ol class="inline">
-            <li>
-                <label for="message" class="hide">Message: </label>
-              <div class="ta-wrap"><textarea id="message" name="message" cols="40" rows="5"></textarea></div>
-            </li>
-            <li class="buttons">
-                <input type="submit" class="submit btn btn-primary" value="send e-mail" /> or <input type="reset" class="reset btn btn-link" value="don't." />
-            </li>
-        </ol>
-        <p class="oldfashioned">or the old way &mdash; <a href="mailto:spaceyraygun@gmail.com">spaceyraygun@gmail.com</a></p>
-        </fieldset>
-    	</form>
-        <p class="inspiration"><small>Inspired by <a href="http://cognition.happycog.com/article/is-this-thing-on" title="Happy Cog's Cognition" rel="external">Happy Cog's Cognition</a>.</small></p>
-    </div>
-    
-    <footer>
-        <p>Hey. What's up?</p>
-    </footer-->
 </div>
 	
   <script>
