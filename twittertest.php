0bdd7f9c21546edc4f437415a6bf9334a35f0271
Kuhn <jrk56@lcbJRK56-iMac.local>
2015-02-12 10:39:21 -0500

    twitter test

--- twittertest.php
+++ twittertest.php
@@ -1,6 +1,6 @@
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:creator" content="@lebow" />
 <meta name="twitter:description" content="Despite an unsuccessful attempt to fund production of their yoga pants via Kickstarter, LeBow students Ashley Revay and Erin Moffitt say they will forge ahead and find a way to bring their product to market." />
-<meta name="twitter:image:src" content="https://www2-dev.lebow.drexel.edu:8040/sites/default/files/1422288391-chakrayoga.jpg" />
+<meta name="twitter:image:src" content="http://www.lebow.drexel.edu/sites/default/files/1422288391-chakrayoga.jpg" />
 <meta name="twitter:site" content="@lebow" />
 <meta name="twitter:title" content="Q&amp;A With Chakra Fitwear’s Erin Moffitt" />
